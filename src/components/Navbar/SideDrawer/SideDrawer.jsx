4e76732956707b1cf57fd166b94063c1517327b4
AlexTicovschi <alexmticovschi@gmail.com>
2019-09-28 09:30:38 +0100

    SideDrawer link change

---
+++ src/components/Navbar/SideDrawer/SideDrawer.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+
+import "./SideDrawer.scss";
+
+const SideDrawer = props => {
+  let drawerClasses = "side-drawer";
+  if (props.show) {
+    drawerClasses = "side-drawer open";
+  }
+
+  return (
+    <nav className={drawerClasses}>
+      <ul>
+        <li>
+          <a className="btn ripple" href="/">
+            Home
+          </a>
+        </li>
+        <li>
+          <a className="btn ripple" href="/movies">
+            Movies
+          </a>
+        </li>
+        <li>
+          <a className="btn ripple" href="/tv-shows">
+            TV Shows
+          </a>
+        </li>
+        <li>
+          <a className="btn ripple" href="/genres/80">
+            Genres
+          </a>
+        </li>
+        <li>
+          <a
+            className="btn ripple"
+            href="https://www.themoviedb.org/login?language=en-US"
+          >
+            Login
+          </a>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default SideDrawer;
