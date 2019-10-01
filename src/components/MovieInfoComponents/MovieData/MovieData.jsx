23d532d7710c592c8fc3d8801b0153639cbf36b4
AlexTicovschi <alexmticovschi@gmail.com>
2019-10-01 11:20:09 +0100

    rebuilding MovieData component

--- src/components/MovieInfoComponents/MovieData/MovieData.jsx
+++ src/components/MovieInfoComponents/MovieData/MovieData.jsx
@@ -1,6 +1,7 @@
 import React from "react";
-
 import Rater from "react-rater";
+import { Link } from "react-router-dom";
+
 import "react-rater/lib/react-rater.css";
 import Fade from "react-reveal/Fade";
 
@@ -9,72 +10,59 @@ import "./movieData.scss";
 const not_available_poster =
   "https://dummyimage.com/342x500/7b8a91/ffffff&text=Poster+Not+Available";
 
-const MovieData = ({ imgPath, base_url, movie, list }) => (
+const MovieData = ({
+  imgPath1,
+  imgPath2,
+  base_url1,
+  base_url2,
+  movie,
+  list
+}) => (
   <section className="movie-data">
-    <div className="movie-data__box-left">
+    <div className="movie-data__inner">
+      <img
+        className="movie-data__backdrop"
+        src={imgPath2 === null ? not_available_poster : base_url2 + imgPath2}
+        alt={"movie-data backdrop"}
+      />
       <Fade>
         <img
-          className="movie-data__img"
-          src={imgPath === null ? not_available_poster : base_url + imgPath}
+          className="movie-data__poster"
+          src={imgPath1 === null ? not_available_poster : base_url1 + imgPath1}
           alt={"movie-data poster"}
         />
-      </Fade>
-    </div>
-    <div className="movie-data__box-right">
-      <div className="">
-        <div className="group movie-info">
-          <div className="group-item line-left"></div>
-          <h1 className="movie-data__title group-item text">
+        <div className="movie-data__details">
+          <h1 className="movie-data__title">
             {movie.original_title}
+            {/* <small>{movie.release_date}</small> */}
           </h1>
-          <div className="group-item line-right"></div>
-        </div>
 
-        {list !== null ? (
+          {list !== null ? (
+            <div className="movie-data__details__genre">
+              <strong>Genre: </strong>
+              {list.map(genre => (
+                <Link
+                  to={`/genres/${genre.id}`}
+                  className="movie-data__details__genre-item"
+                >
+                  {genre.name}
+                </Link>
+              ))}
+            </div>
+          ) : null}
+          <div className="movie-data__star-rating">
+            <strong>Rating: </strong>
+            <Rater
+              interactive={false}
+              total={5}
+              rating={movie.vote_average / 2}
+            />
+          </div>
           <p>
-            <strong>Genre:</strong> {list}
+            <strong>Released: </strong> {movie.release_date}
           </p>
-        ) : null}
-        <div className="movie-data__star-rating">
-          <strong>Rating: </strong>
-          <Rater
-            interactive={false}
-            total={5}
-            rating={movie.vote_average / 2}
-          />
         </div>
-
-        <p>
-          <strong>Released: </strong> {movie.release_date}
-        </p>
-        <p>
-          <strong>Overview: </strong> {movie.overview}
-        </p>
-        {movie.BoxOffice ? (
-          <p>
-            <strong>BoxOffice: </strong> {movie.BoxOffice}
-          </p>
-        ) : null}
-        {movie.homepage ? (
-          <p>
-            <strong>Website: </strong>{" "}
-            <a href={movie.homepage} target="_blank" rel="noopener noreferrer">
-              {movie.original_title} Official Website
-            </a>
-          </p>
-        ) : null}
-
-        <div className="btn-movie-info">
-          <a
-            className="button movie-info"
-            href={`http://imdb.com/title/${movie.imdb_id}`}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            View on IMDB
-          </a>
-        </div>
-      </div>
+      </Fade>
     </div>
   </section>
 );
