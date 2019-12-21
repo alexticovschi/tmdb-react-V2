d4842ec24adc0309053b62560e463d766e5174a7
AlexTicovschi <alexmticovschi@gmail.com>
2019-12-21 13:14:56 +0000

    removing unnecessary code

--- src/routes.jsx
+++ src/routes.jsx
@@ -20,20 +20,6 @@ import Person from './Pages/Person/Person';
 
 import Footer from './components/Footer/Footer';
 
-// import MovieInfo from "./containers/MovieInfo/MovieInfo";
-// import ActorProfileInfo from "./containers/ActorProfileInfo/ActorProfileInfo";
-// import ActorImages from "./components/ActorImages/ActorImages";
-// import TVShows from "./containers/TVShows/TVShows";
-// import TVShowInfo from "./containers/TVShowInfo/TVShowInfo";
-// import PopularTvShows from "./containers/PopularTvShows/PopularTvShows";
-// import NowOnTheAirTVShows from "./containers/NowOnTheAirTVShows/NowOnTheAirTVShows";
-// import TopRatedTvShows from "./containers/TopRatedTvShows/TopRatedTvShows";
-// import LatestTvShows from "./containers/LatestTvShows/LatestTvShows";
-// import NowPlayingMovies from "./containers/NowPlayingMovies/NowPlayingMovies";
-// import PopularMovies from "./containers/PopularMovies/PopularMovies";
-// import TopRatedMovies from "./containers/TopRatedMovies/TopRatedMovies";
-import MovieGenres from './containers/MovieGenres/MovieGenres';
-
 import { Switch, Route } from 'react-router-dom';
 import { withRouter } from 'react-router-dom';
 import ScrollUpButton from 'react-scroll-up-button';
@@ -73,37 +59,6 @@ class Routes extends Component {
           <Route exact path='/genres' component={Genres} />
           <Route exact path='/genres/:genre_id' component={Genres} />
           <Route exact path='/person/:person_id' component={Person} />
-
-          {/* <Route exact path="/movies/now-playing" component={Movies} />
-          <Route exact path="/movies/popular" component={PopularMovies} />
-          <Route exact path="/movies/top-rated" component={TopRatedMovies} />
-          <Route exact path="/genres" component={MovieGenres} />{" "}
-          <Route exact path="/genres/:genre_id" component={MovieGenres} />
-          <Route exact path="/tv-shows" component={TVShows} />
-          <Route exact path="/tv-shows/popular" component={PopularTvShows} />
-          <Route
-            exact
-            path="/tv-shows/now-on-the-air"
-            component={NowOnTheAirTVShows}
-          />
-          <Route exact path="/tv-shows/top-rated" component={TopRatedTvShows} />
-          <Route exact path="/tv-shows/latest" component={LatestTvShows} />
-          <Route
-            exact
-            path="/tv-show-info/:tv_show_id"
-            component={TVShowInfo}
-          /> */}
-          {/* <Route exact path="/movie-info/:movie_id" component={MovieInfo} /> */}
-          {/* <Route
-            exact
-            path="/cast/actor-profile-info/:actor_id"
-            component={ActorProfileInfo}
-          />
-          <Route
-            exact
-            path="/cast/:actor_id/images/profiles"
-            component={ActorImages}
-          /> */}
         </Switch>
 
         <ScrollUpButton ContainerClassName='scroll-up-button' />
