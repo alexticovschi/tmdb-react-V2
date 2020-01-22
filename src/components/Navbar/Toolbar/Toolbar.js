57777c496ce3632c06afda5379e841f0b7dd0a00
AlexTicovschi <alexmticovschi@gmail.com>
2020-01-22 10:36:52 +0000

    changing 'a' tag to react Link

--- src/components/Navbar/Toolbar/Toolbar.js
+++ src/components/Navbar/Toolbar/Toolbar.js
@@ -8,12 +8,12 @@ const Toolbar = props => (
     <div className='toolbar-wrapper'>
       <nav className='toolbar__navigation'>
         <div className='toolbar__logo'>
-          <a href='/'>
+          <Link to='/'>
             <img
               src='https://www.themoviedb.org/assets/1/v4/logos/powered-by-rectangle-green-dcada16968ed648d5eb3b36bbcfdd8cdf804f723dcca775c8f2bf4cea025aad6.svg'
               alt=''
             />
-          </a>
+          </Link>
         </div>
         <div className='spacer' />
         <div className='toolbar_navigation-items'>
