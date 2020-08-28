2612ef0c9cc47825f604709fd7628870e827f950
Alex Ticovschi <alexmticovschi@gmail.com>
2020-08-28 14:07:29 +0100

    Netlify Deploy failed fix attempt

--- src/Pages/Genres/Genres.jsx
+++ src/Pages/Genres/Genres.jsx
@@ -1,4 +1,4 @@
-import React, { Component, useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import MovieList from "../../components/MoviesComponents/Movies/Movies";
 
 import Select from "react-select";
