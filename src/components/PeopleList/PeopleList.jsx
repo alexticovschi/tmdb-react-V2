e710b6458d0806e7e118168c5f12b472c4586dfd
Alex Ticovschi <alexmticovschi@gmail.com>
2020-08-29 20:37:06 +0100

    adding minimum height to PeopleList component

--- src/components/PeopleList/PeopleList.jsx
+++ src/components/PeopleList/PeopleList.jsx
@@ -1,12 +1,17 @@
-import React from 'react';
-import PeopleItem from '../PeopleItem/PeopleItem';
-import './PeopleList.scss';
+import React from "react";
+import PeopleItem from "../PeopleItem/PeopleItem";
+import "./PeopleList.scss";
 
 const PeopleList = ({ people }) => {
   return (
     <div className="people-list">
       {people.map((person) => (
-        <PeopleItem key={person.id} profile_path={person.profile_path} id={person.id} name={person.name} />
+        <PeopleItem
+          key={person.id}
+          profile_path={person.profile_path}
+          id={person.id}
+          name={person.name}
+        />
       ))}
     </div>
   );
